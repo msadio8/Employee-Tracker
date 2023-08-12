b9f542e57fc81f40527096dac18578c73ad18f4d
Meriam <132971614+msadio8@users.noreply.github.com>
2023-08-12 12:50:37 +1000

    add info in seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+
+INSERT INTO salary (title , salary)
+VALUES ()
+
+iNSERT INTO employees_name (first_name, last_name)
+VALUES ("01 Bad Pitt"),
+       ("02 Angelina Jolie"),
+       ("03 Taylor Swift"),
+       ("04 Jackie Chan"),
+       ("05 Emilia Clark"),
+       ("06 Julia Roberts"),
+       ("07 Johny Deep"),
+       ("08 Leonardo DiCarpio"),
+       ("09 Charlize Theron");
+       
