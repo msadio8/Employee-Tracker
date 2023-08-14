2441987d5a36ffa3346cbb8edab4f53d7fb7f947
Meriam <132971614+msadio8@users.noreply.github.com>
2023-08-14 13:28:52 +1000

    role function added

--- db/roles.js
+++ db/roles.js
@@ -1,1 +1,92 @@
 const {promptUser} = require('./prompt');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+   {
+      host: "localhost",
+       // MySQL username,
+      user: "root",
+       // MySQL password
+      password: "winter123",
+      database: "employees_db",
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+
+const viewRoles = () => {
+    db.query(
+        `SELECT roles.id, roles.title, roles.salary, department.name
+           FROM roles
+           LEFT JOIN departmment
+           ON roles.department_id = department.id`,
+        function (err, results, fields) {
+            if (err) {
+                console.log(err.message);
+                return;
+            }
+            console.table(results);
+            promptUser();
+        }
+
+    );
+};
+
+const addRole = () => {
+    db.query(
+        `SELECT * FROM department`,
+        function (err, results, fields) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            let depArr = [];
+            results.forEach(item =>{
+                depArr.push(item.name)
+            })
+
+            inquirer
+                .prompt([
+                    {
+                        type: 'text',
+                        name: 'role_title',
+                        message: 'Please enter the name of the role you want to add:'
+                    },
+                    {
+                        type: 'amount',
+                        name: 'salary',
+                        message: 'Please enter nominated salary for this role.(no commas or periods)'
+                    },
+                    {
+                        type: 'list',
+                        name: 'department',
+                        message: 'Please select the department for role',
+                        choices: depArr
+                    }
+                ])
+                .then((data) => {
+                    let department_id;
+
+                    for (let i = 0; i <depArr.length; i++) {
+                        if (depArr[i] === data.department) {
+                            department_id = i + 1;
+                        };
+                    };
+                    db.query(
+                        `INSERT INTO roles (title, salary, department_id)
+                           VALUES(?,?,?)`,
+                        [data.role_title, data.salary, department_id],
+                        function (err, results, fields) {
+                            if (err) {
+                                console.log(err.message);
+                                return;
+                            }
+                            console.log('New Role added!');
+                            promptUser();
+                        }
+                    );
+                });
+        }
+    );
+};
+module.exports = { viewRoles, addRole};
--- db/schema.sql
+++ db/schema.sql
@@ -8,13 +8,19 @@ DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employee;
 
+CREATE TABLE manager (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+);
+
 CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY
   name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id: INT NOT NULL PRIMARY KEY
+    id: INT AUTO_INCREMENT NOT NULL PRIMARY KEY
     title: VARCHAR(30)
     salary: DECIMAL
     department_id: INT 
@@ -22,7 +28,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id: INT NOT NULL PRIMARY KEY
+    id: INT AUTO_INCREMENT NOT NULL PRIMARY KEY
     first_name: VARCHAR(30) 
     last_name: VARCHAR(30)
     role_id: INT 
