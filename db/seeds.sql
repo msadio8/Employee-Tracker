6c8459e3ed744e374a5b5e58e8842fbe0ccf0db6
Meriam <132971614+msadio8@users.noreply.github.com>
2023-08-15 22:52:41 +1000

    fix the bug

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,7 +6,7 @@ INSERT INTO roles (title , salary, department_id)
 VALUES ('Legal Team Lead', 250000, 1),
     ('Lawyer', 190000, 1),
     ('Salesperson', 80000, 2),
-    ('Sales Lead', 100000, 2)
+    ('Sales Lead', 100000, 2),
     ('Lead Engineer', 120000, 3),
     ('Engineer', 150000, 3);
 INSERT INTO employee ( first_name,last_name, role_id, manager_id, manager_confirm)                           
@@ -20,8 +20,8 @@ VALUES ('Brad', 'Pitt', 1, null, true),
     ('Leonardo', 'DiCarpio', 5, null, true),
     ('Mickey', 'Mouse', 6, 3, false),
     ('Charlize', 'Theron', 6, 3, false);
-INSERT INTO manager(first_name, last_name)
-SELECT first_name,
-    last_name
-FROM employee
-WHERE manager_confirm = 1;
+-- INSERT INTO manager(first_name, last_name)
+-- SELECT first_name,
+--     last_name
+-- FROM employee
+-- WHERE manager_confirm = 1;
